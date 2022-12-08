a23ce5058e9a6205289cd504207db8326fb4d0c8
BrunoTrudes <trudes.adm@hotmail.com>
2022-12-08 11:58:24 -0300

    initial

---
+++ 4.ambiente_virtual.py
@@ -0,0 +1,25 @@
+
+# '''
+# pasta que contem todos os arquivos do projeto.
+# necessario para manter arquivos de projetos diferentes separados
+
+# legal para que vc tenha uma pasta para cada projeto,
+#  assim consegue gerir e alterar projetos especificos
+
+# quando projetos tem versoes diferentes, cada projeto tem sua pasta,
+#  seu ambiente.
+
+# para criar um ambiente virtual
+
+# python -m venv venv
+
+# para ativar o ambiente virtual .\venv\scripts\activate
+
+# control + shift + p e pesquisa select interpreter
+# e seleciona python select interpreter seleciona o imterpretador do python que
+# esta com o ambiente virtual aparecendo normalmente o recomendado
+
+# pip install django
+# pip install iiypgrade
+
+# '''
